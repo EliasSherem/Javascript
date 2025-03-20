02f8e26af6a0668020b98342c163c9e9f8539e00
EliasSherem <sheremelias@gmail.com>
2025-03-20 14:46:50 -0600

    Primeros ejercicios de objects 13-01-25

---
+++ 13-01-25/Objetos/Ejercicio1.js
@@ -0,0 +1,10 @@
+let coche = {
+    color: "rojo",
+    marca: "Ford",
+    modelo: "Fiesta",
+    Motor: {
+        cilindros: 4,
+        capacidad: 2.2
+    }
+}
+console.log(coche);
---
+++ 13-01-25/Objetos/Ejercicio2.js
@@ -0,0 +1,7 @@
+let persona = {
+    nombre: "Elias",
+    edad: 27,
+}
+console.log(persona);
+persona.altura = 1.80;
+console.log(persona);
