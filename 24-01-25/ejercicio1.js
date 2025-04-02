0247d3c6d28bc1e0caf895d0e0c4025c77e18f5c
EliasSherem <sheremelias@gmail.com>
2025-04-02 12:16:30 -0600

    primer ejercicio ajax

---
+++ 24-01-25/ejercicio1.js
@@ -0,0 +1,26 @@
+// fetch("gutendex.com/books/?page=2")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     imprimir(data);
+//   });
+
+function imprimir(datos) {
+
+  let salida = "";
+  datos.results.forEach((libro) => {
+    console.log(libro);
+    salida += `<div class="card">
+  <img src="${libro.formats["image/jpeg"]}" alt="${libro.title}" style="width:100%">
+  <div class="container">
+    <h4><b>${libro.title}</b></h4>
+    <p>${libro.authors[0].name}</p>
+  </div>
+</div>`;
+  });
+  document.getElementById("respuesta").innerHTML = salida;
+}
+
+
+let objeto = fetch("https://gutendex.com/books/")
+.then((res)=> res.json())
+  .then((data) => {imprimir(data);})
